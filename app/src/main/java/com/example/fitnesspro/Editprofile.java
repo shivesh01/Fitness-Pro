4b17d6a97dc547e78ae4093b68eb860c36beb67e
shivesh01 <singhshiveshkumar@gmail.com>
2021-02-27 16:19:21 +0530

    ADDING ALL FILES

---
+++ app/src/main/java/com/example/fitnesspro/Editprofile.java
@@ -0,0 +1,28 @@
+package com.example.fitnesspro;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+
+import java.util.HashMap;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class Editprofile extends AppCompatActivity {
+
+
+
+}
